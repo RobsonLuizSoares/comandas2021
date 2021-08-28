4eb5d5d4cc7380a8c3efbf5ceb62d3ba3e60e302
Robson Luiz <robsonptrainer@gmail.com>
2021-08-28 13:54:31 -0300

    fixed strategy persist user data

--- config/firebaseClient/index.js
+++ config/firebaseClient/index.js
@@ -16,7 +16,7 @@ const app = firebase.apps.length
   ? firebase.app()
   : firebase.initializeApp(firebaseConfig)
 
-export const persistenceMode = firebase.auth.Auth.Persistence.SESSION
+export const persistenceMode = firebase.auth.Auth.Persistence.LOCAL
 
 export default app
 
